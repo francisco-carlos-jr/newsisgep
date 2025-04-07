57a69b882e9109ec0a6cc83e845ae338b3ce23b2
root <francisco.c9001@edu.pucrs.br>
2025-04-07 23:25:10 +0000

    deixei funcionando

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const usuarioRoutes = require('./routes/sentenciadoRoutes');
 require('dotenv').config();
 
 app.use(express.json());
-app.use('/sentenciado', sentenciadoRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/sentenciado', sentenciadoRoutes);
 
-app.listen(process.env.PORT, () => {
+/*app.listen(process.env.PORT, () => {
   console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
-});
+});*/
 
 app.listen(process.env.PORT, '0.0.0.0', () => {
     console.log(`Servidor rodando...`);
   });
-
-console.log('teste')
